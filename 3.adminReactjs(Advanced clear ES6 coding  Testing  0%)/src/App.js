f39167bbbe326ff77990c124b3595dd886128e8a
walid <walid.info27@gmail.com>
2020-05-10 03:54:09 +0200

    first commit

---
+++ 3.adminReactjs(Advanced clear ES6 coding  Testing  0%)/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const App = () => {
+
+  return (
+    <div>
+      hallo Admin
+	  </div>);
+
+}
+
+const AppWithStore = () => {
+  return(<App />)
+
+}
+
+export default AppWithStore;
--- README.md
+++ README.md
@@ -1,7 +1,7 @@
  
  
 ### Reactjs  + ReactNative 
-<img src='plan.png' width='600px' height='350px'>
+<img src='plan.png' width='800px' height='450px'>
 
 
 
